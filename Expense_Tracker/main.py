2255c6ba4ad951778103cb78ba972a4326447805
Dee <deesh004@gmail.com>
2026-07-27 21:17:44 +0400

    Using pandas to load the csv file

--- Expense_Tracker/main.py
+++ Expense_Tracker/main.py
@@ -6,3 +6,22 @@ import os
 
 st.set_page_config(page_title="Expense Tracker", page_icon="💰", layout="wide")
 
+# st.write("Hello World")
+
+def load_transactions(file):
+    try:
+        df = pd.read_csv(file)
+        st.write(df)
+    except Exception as e:
+        st.error(f"Error processing file: {str(e)}")
+        return None
+
+def main():
+    st.title("Expense Dashboard")
+
+    uploaded_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
+
+    if uploaded_file is not None:
+        df = load_transactions(uploaded_file)
+
+main()
